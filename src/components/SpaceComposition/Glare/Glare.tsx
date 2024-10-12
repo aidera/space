556b76d81c6c69bd89d8a8e0064f4caf810496d6
aidera <hope.aidera@gmail.com>
2024-10-12 14:13:44 -0400

    Better glare animation

--- src/components/SpaceComposition/Glare/Glare.tsx
+++ src/components/SpaceComposition/Glare/Glare.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 import styles from './Glare.module.scss';
 
 export default function Glare() {
-  return <div className={styles.glare}></div>;
+  const glareRef = useRef(null);
+
+  useEffect(() => {
+    const glareElement = glareRef.current;
+
+    gsap.fromTo(glareElement, { opacity: 0 }, { opacity: 1, duration: 1 });
+
+    gsap.to(glareElement, {
+      repeat: -1,
+      yoyo: true,
+      duration: 10,
+      delay: 1.1,
+      keyframes: {
+        opacity: [1, 0.85, 1],
+      },
+    });
+  }, []);
+
+  return <div className={styles.glare} ref={glareRef}></div>;
 }
