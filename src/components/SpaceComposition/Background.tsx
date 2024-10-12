20ef10911c742392dbc49334a6a0eac7c240dfab
aidera <hope.aidera@gmail.com>
2024-10-12 14:23:55 -0400

    Background animation added

--- src/components/SpaceComposition/Background.tsx
+++ src/components/SpaceComposition/Background.tsx
@@ -1,4 +1,5 @@
-import { useLoader } from '@react-three/fiber';
+import { useRef, useEffect } from 'react';
+import { useFrame, useLoader } from '@react-three/fiber';
 import {
   LinearFilter,
   NoColorSpace,
@@ -8,13 +9,13 @@ import {
   MeshBasicMaterial,
   Mesh,
 } from 'three';
+import { gsap } from 'gsap';
 
 export default function Background() {
   const texture = useLoader(
     TextureLoader,
     `${import.meta.env.BASE_URL}stars.png`
   );
-
   texture.wrapS = RepeatWrapping;
   texture.wrapT = RepeatWrapping;
   texture.repeat.set(1.8, 1.8);
@@ -30,9 +31,27 @@ export default function Background() {
   });
   const mesh = new Mesh(geometry, material);
 
+  const meshRef = useRef(mesh);
+
+  useEffect(() => {
+    gsap.to(meshRef.current.material, {
+      opacity: 0.7,
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+    });
+  }, [meshRef.current]);
+
+  useFrame((_, delta) => {
+    if (meshRef.current) {
+      meshRef.current.position.x += 0.01 * delta;
+      meshRef.current.position.y += 0.01 * delta;
+    }
+  });
+
   return (
     <primitive
-      object={mesh}
+      object={meshRef.current}
       position={[0, 0, -5]} // Adjust position to place it appropriately in the background
       scale={[10, 10, 10]}
     />
